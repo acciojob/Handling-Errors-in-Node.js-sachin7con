f8efd467c3f926667f26f56aef62a97a214a7d7e
sachin7con <sachin7con@gmail.com>
2024-12-28 16:32:24 +0530

    First commit

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
+//SGN, ONS, JSLN, JBB, JSSR, JSRK, JSSM, JMD, JSVM, JSP, JSRK
 const fs = require('fs');
 
 function printFileContents(filePath) {
   // TODO: Use fs.readFile to read the file contents
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if(err) {
+        if(err.code === "ENOENT"){
+          console.log(`Column '${filePath}' not found in the CSV.`);
+        }        else {
+          console.log('Error reading the file:', err.message);
+        }
+    } else {
+      console.log(data);
+    }
+  })
+
+
 }
 
+const filePath = process.argv[2];
+if(!filePath){
+  console.log("Please provide a file path.")
+  process.exit(1);
+}
 // TODO: Call printFileContents with the command-line argument
+
+printFileContents(filePath);
